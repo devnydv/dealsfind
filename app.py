cdeefdfac421cce586a216bfe744f19840f37cbb
devnydv <80939011+devnydv@users.noreply.github.com>
2024-08-23 23:46:06 +0530

    Update app.py
    deals page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import os
 import json
+from bs4 import BeautifulSoup
+import requests
 from hitapi import hitfirebase, getdeals
 from getdetails import getfirebase
 
@@ -38,7 +40,18 @@ def detail():
     #print(itemdata)
     return render_template("details.html", data = menudata, id=id, cate= cate, itemdata = itemdata)
 
+@app.route("/deals")
+def deals():
 
+    url = "https://inrdeals.com/ajax/deals/engine?page=1&user=bit422097711"
+    #url = "https://www.flipkart.com/realme-c53-champion-black-128-gb/p/itm5df90168ecd05?pid=MOBGTEVGGM7CTGXU"
+    head = ({
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Accept-Language":"en-US , en;q=0.5"
+})
+    data = requests.get(url, headers=head)
+    soup = BeautifulSoup(data.content, "html.parser")
+    return render_template("deals.html", deals= soup)
 
 
 #if __name__ == "__main__":
